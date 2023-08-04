405ef646e227ee9f55eb0aa09f7e587e5355c141
LavenderJenkins <kimlienc15@gmail.com>
2023-08-05 01:17:11 +0700

    first commit

---
+++ flutter_application_2/lib/screens/chats/components/body.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/components/filled_outline_button.dart';
+import 'package:flutter_application_2/constants.dart';
+import 'package:flutter_application_2/models/Chat.dart';
+import 'package:flutter_application_2/screens/messages/message_screen.dart';
+
+import 'chat_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(
+              kDefaultPadding, 0, kDefaultPadding, kDefaultPadding),
+          color: kPrimaryColor,
+          child: Row(
+            children: [
+              FillOutlineButton(press: () {}, text: "Recent Message"),
+              const SizedBox(width: kDefaultPadding),
+              FillOutlineButton(
+                press: () {},
+                text: "Active",
+                isFilled: false,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: chatsData.length,
+            itemBuilder: (context, index) => ChatCard(
+              chat: chatsData[index],
+              press: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MessagesScreen(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
